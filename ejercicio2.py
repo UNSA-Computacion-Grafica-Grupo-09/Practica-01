af47c86eb11496b88597c11f036867509c0d96b1
Williamhyuk <williamhyuk@gmail.com>
2020-04-29 11:52:50 -0500

    Ando avanzando

---
+++ ejercicio2.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('thresh1.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+neg=255-gray
+
+cv2.imshow('image', img)
+cv2.imshow('gray', gray)
+cv2.imshow('neg',neg)
+
+_,threshold_binary = cv2.threshold(neg,90,150,cv2.THRESH_BINARY)
+cv2.imshow('thresholdin',threshold_binary)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
